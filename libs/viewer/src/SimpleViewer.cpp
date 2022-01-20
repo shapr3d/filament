a3c30c731b86e7c42561895c86177ac2488b0af4
Viktor Heisenberger <viktor.heisenberger@shapr3d.com>
2022-01-20 19:11:32 +0100

    Revert Z-up changes

--- libs/viewer/src/SimpleViewer.cpp
+++ libs/viewer/src/SimpleViewer.cpp
@@ -598,10 +598,7 @@ void SimpleViewer::updateIndirectLight() {
     using namespace filament::math;
     if (mIndirectLight) {
         mIndirectLight->setIntensity(mSettings.lighting.iblIntensity);
-        mIndirectLight->setRotation(
-            mat3f::rotation(mSettings.lighting.iblRotation, float3{ 0, 0, 1 }) *
-            mat3f::rotation((float)M_PI_2, float3{ 1, 0, 0 })
-        );
+        mIndirectLight->setRotation(mat3f::rotation(mSettings.lighting.iblRotation, float3{ 0, 1, 0 }));
     }
     if (mScene->getSkybox()) {
         mScene->getSkybox()->setIntensity(mSettings.lighting.skyIntensity);
