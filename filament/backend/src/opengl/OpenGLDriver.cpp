374038f146f2012b964a1bf9b6a7ac1d652f9798
Mathias Agopian <mathias@google.com>
2022-02-18 10:38:27 -0800

    Workaround feedback loops limitation in GLES

--- filament/backend/src/opengl/OpenGLDriver.cpp
+++ filament/backend/src/opengl/OpenGLDriver.cpp
@@ -1585,6 +1585,8 @@ bool OpenGLDriver::isWorkaroundNeeded(Workaround workaround) {
     switch (workaround) {
         case Workaround::SPLIT_EASU:
             return mContext.bugs.split_easu;
+        case Workaround::ALLOW_READ_ONLY_ANCILLARY_FEEDBACK_LOOP:
+            return mContext.bugs.allow_read_only_ancillary_feedback_loop;
     }
     return false;
 }
