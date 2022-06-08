faf71244ac74c0a80c7d6f9ee9c97e6a77c1831f
marcisolti <marci.solti.hu@gmail.com>
2022-06-08 08:00:05 +0200

    disable warning for bitshift overflow

--- libs/utils/include/utils/algorithm.h
+++ libs/utils/include/utils/algorithm.h
@@ -47,12 +47,15 @@ constexpr inline T clz(T x) noexcept {
     x |= (x >> 4u);
     x |= (x >> 8u);
     x |= (x >> 16u);
+#pragma warning(push)
+#pragma warning(disable : 4293)
     if (sizeof(T) * CHAR_BIT >= 64) {   // just to silence compiler warning
         x |= (x >> 32u);
     }
     if (sizeof(T) * CHAR_BIT >= 128) {   // just to silence compiler warning
         x |= (x >> 64u);
     }
+#pragma warning(pop)
     return T(sizeof(T) * CHAR_BIT) - details::popcount(x);
 }
 
