4e4d8567b3a3fe9c59021817764530ac4b9e812d
Gabor Valasek <gabor.valasek@shapr3d.com>
2023-02-17 16:25:24 +0100

    [Changed] Maximum and minimum scale of roughness scaler

--- libs/viewer/src/TweakableMaterial.cpp
+++ libs/viewer/src/TweakableMaterial.cpp
@@ -265,7 +265,7 @@ void TweakableMaterial::drawUI(const std::string& header) {
         mNormal.addWidget("normal");
         if (mNormal.isFile) enqueueTextureRequest(mNormal, false, false, 3);
 
-        mRoughnessScale.addWidget("roughness scale", 0.0f, 3.0f);
+        mRoughnessScale.addWidget("roughness scale", 0.125f, 2.0f);
         mRoughnessUvScaler.addWidget("roughness UV multiplier", 1.0f, 8.0f);
 
         mRoughness.addWidget("roughness");
